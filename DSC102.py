435a6c862bfaefc3e27b2acd12bacd66119ca7ac
Sophia Vo <sovo@ucsd.edu>
2025-12-01 16:34:05 -0800

    Implement task_2 to process product data

--- DSC102.py
+++ DSC102.py
@@ -78,3 +78,132 @@ def task_1(data_io, review_data, product_data):
     data_io.save(res, 'task_1')
     return res
     # -------------------------------------------------------------------------
+
+
+
+
+
+
+
+TASK 2 HERE:
+
+# %load -s task_2 assignment2.py
+def task_2(data_io, product_data):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    salesRank_column = 'salesRank'
+    categories_column = 'categories'
+    asin_column = 'asin'
+    # Outputs:
+    category_column = 'category'
+    bestSalesCategory_column = 'bestSalesCategory'
+    bestSalesRank_column = 'bestSalesRank'
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Your implementation begins------------------------
+
+    cat_col = F.col(categories_column)
+    product_flat = (
+        product_data
+        .withColumn(
+            category_column,
+            F.when(
+                cat_col.isNull() | (F.size(cat_col) == 0),
+                F.lit(None)
+            ).otherwise(
+                cat_col.getItem(0).getItem(0)
+            )
+        )
+    )
+
+    # 2) Flatten salesRank map into bestSalesCategory (key) and bestSalesRank (value)
+    sr_col = F.col(salesRank_column)
+    product_flat = (
+        product_flat
+        .withColumn(
+            bestSalesCategory_column,
+            F.when(
+                sr_col.isNull() | (F.size(F.map_keys(sr_col)) == 0),
+                F.lit(None)
+            ).otherwise(
+                F.map_keys(sr_col).getItem(0)
+            )
+        )
+        .withColumn(
+            bestSalesRank_column,
+            F.when(
+                sr_col.isNull() | (F.size(F.map_values(sr_col)) == 0),
+                F.lit(None)
+            ).otherwise(
+                F.map_values(sr_col).getItem(0)
+            )
+        )
+    )
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'count_total': None,
+        'mean_bestSalesRank': None,
+        'variance_bestSalesRank': None,
+        'numNulls_category': None,
+        'countDistinct_category': None,
+        'numNulls_bestSalesCategory': None,
+        'countDistinct_bestSalesCategory': None
+    }
+    # Modify res:
+
+    count_total = product_flat.count()
+
+    # Mean and variance of bestSalesRank (nulls ignored)
+    stats_row = product_flat.agg(
+        F.mean(bestSalesRank_column).alias('mean_bestSalesRank'),
+        F.variance(bestSalesRank_column).alias('variance_bestSalesRank')
+    ).collect()[0]
+
+    # Null counts
+    num_nulls_category = product_flat.filter(F.col(category_column).isNull()).count()
+    num_nulls_bestSalesCategory = product_flat.filter(F.col(bestSalesCategory_column).isNull()).count()
+
+    # Distinct counts excluding nulls
+    count_distinct_category = (
+        product_flat
+        .select(category_column)
+        .where(F.col(category_column).isNotNull())
+        .distinct()
+        .count()
+    )
+
+    count_distinct_bestSalesCategory = (
+        product_flat
+        .select(bestSalesCategory_column)
+        .where(F.col(bestSalesCategory_column).isNotNull())
+        .distinct()
+        .count()
+    )
+
+    # Fill res with native Python types
+    res['count_total'] = int(count_total)
+
+    mean_bsr = stats_row['mean_bestSalesRank']
+    var_bsr = stats_row['variance_bestSalesRank']
+
+    res['mean_bestSalesRank'] = float(mean_bsr) if mean_bsr is not None else None
+    res['variance_bestSalesRank'] = float(var_bsr) if var_bsr is not None else None
+
+    res['numNulls_category'] = int(num_nulls_category)
+    res['countDistinct_category'] = int(count_distinct_category)
+    res['numNulls_bestSalesCategory'] = int(num_nulls_bestSalesCategory)
+    res['countDistinct_bestSalesCategory'] = int(count_distinct_bestSalesCategory)
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_2')
+    return res
+    # -------------------------------------------------------------------------
+
